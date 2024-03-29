8ec6c652415db47e2a69da2d0c8142b3bc305f38
David Rubin <daviru007@icloud.com>
2024-03-29 06:48:41 -0700

    implement slices

--- src/codegen/llvm.zig
+++ src/codegen/llvm.zig
@@ -11074,6 +11074,7 @@ fn lowerFnRetTy(o: *Object, fn_info: InternPool.Key.FuncType) Allocator.Error!Bu
                             }
                             return o.builder.structType(.normal, types[0..types_len]);
                         },
+                        .none => unreachable,
                     }
                 },
                 // TODO investigate C ABI for other architectures
@@ -11316,6 +11317,7 @@ const ParamTypeIterator = struct {
                                 it.llvm_index += it.types_len - 1;
                                 return .multiple_llvm_types;
                             },
+                            .none => unreachable,
                         }
                     },
                     // TODO investigate C ABI for other architectures
